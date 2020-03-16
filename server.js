1f1880da556b600a04ee9e4660ef693b6fd64367
EnriqueAC <enrique.arreolac@gmail.com>
2020-03-16 09:33:37 -0600

    Update wirh Record_Firmas table

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('Cat_Usuarios'); 
-mobileApp.tables.add('Record_Clasificacion'); 
+mobileApp.tables.add('Record_Clasificacion');
+mobileApp.tables.add('Record_Firmas'); 
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
