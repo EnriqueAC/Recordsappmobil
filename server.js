2b4dd124f9ec72bcea75fc7578d9527885e2908b
EnriqueAC <enrique.arreolac@gmail.com>
2020-03-16 02:27:33 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Book'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('Cat_Usuarios'); 
+mobileApp.tables.add('Record_Clasificacion'); 
+mobileApp.tables.add('Record_Firmas'); 
+mobileApp.tables.add('Record_Procedimiento'); 
+mobileApp.tables.add('Record_RedimientosMateriales'); 
+mobileApp.tables.add('Record_Rendimientos'); 
+mobileApp.tables.add('Record_resultado'); 
+mobileApp.tables.add('Record_ResultadoImagenes'); 
+mobileApp.tables.add('Records_AspectoSeguridad'); 
+
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
