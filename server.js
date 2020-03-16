624592435e36d60549fd38dd7d900a979192e164
EnriqueAC <enrique.arreolac@gmail.com>
2020-03-16 08:56:05 -0600

    Update with Record_Clasificacion table

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('Cat_Usuarios'); 
-
+mobileApp.tables.add('Record_Clasificacion'); 
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
