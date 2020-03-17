1718e197d9c58b79669e434841963b88044515f9
EnriqueAC <enrique.arreolac@gmail.com>
2020-03-17 12:52:37 -0600

    update with Records_AspectoSeguridad table

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mobileApp.tables.add('Record_RedimientosMateriales');
 mobileApp.tables.add('Record_Rendimientos'); 
 mobileApp.tables.add('Record_resultado'); 
 mobileApp.tables.add('Record_ResultadoImagenes'); 
-
+mobileApp.tables.add('Records_AspectoSeguridad'); 
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
